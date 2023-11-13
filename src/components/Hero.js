c484d6e768566fa4eb481412a042b69aabcdfddf
AgonoDavid <davidagono239@gmail.com>
2023-11-13 22:04:05 +0100

    Navbar & Hro completed

---
+++ src/components/Hero.js
@@ -0,0 +1,47 @@
+import React from "react";
+import heroImage from "../assets/Group 16.png";
+import { Button, Divider } from "@chakra-ui/react";
+
+const Hero = () => {
+  return (
+    <section className="bg-[#F2F0F1] mt-2">
+      <main className="max-w-[1100px] m-auto flex justify-between items-center w-full">
+        <div className="w-[50%]">
+          <div className=" py-3">
+            <p className="font-Teko text-[90px] leading-[75px]">
+              FIND CLOTHES THAT MATCHES YOUR STYLE
+            </p>
+            <p className=" text-[14px]">
+              Browse through our meticulously crafted garments, designed to
+              bring out your individuality and cater to your sense of style
+            </p>
+          </div>
+          <Button bg={"black"} textColor={"white"}>
+            Shop Now
+          </Button>
+          <div className=" flex justify-between w-[90%] h-[85px] py-3 ">
+            <div>
+              <p className=" text-slate-950 font-semibold text-3xl">200+</p>
+              <p>International Brands</p>
+            </div>
+            <Divider orientation="vertical" border={"1px solid #000000"} />
+            <div>
+              <p className=" text-slate-950 font-semibold text-3xl">2,000+</p>
+              <p>High-Quality Products</p>
+            </div>
+            <Divider orientation="vertical" border={"1px solid #000000"} />
+            <div>
+              <p className=" text-slate-950 font-semibold text-3xl">30,000+</p>
+              <p>Happy Customers</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-[50%]">
+          <img src={heroImage} alt="heroimage" />
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "../assets/SHOP.CO.png";
+import searchIcon from "../assets/Frame (2).png";
+import cartIcon from "../assets/Frame.png";
+import profileIcon from "../assets/Frame (1).png";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <main className=" max-w-[1100px] m-auto flex justify-between w-full py-[30px]">
+        <div className=" flex items-center w-[15%]">
+          <img src={logo} alt="logo" className=" align-middle w-[100%]" />
+        </div>
+        <div className="flex items-center w-[35%] justify-around">
+          <p>shop</p>
+          <p>On sale</p>
+          <p>New Arrivals</p>
+          <p>Brands</p>
+        </div>
+        <div className="flex items-center rounded-[20px] px-2 py-1 w-[50%] bg-[#F0F0F0]">
+          <div className=" w-[10%]">
+            <img src={searchIcon} alt="searchIcon" className=" w-[50%]" />
+          </div>
+          <div className=" w-[90%] ">
+            <input
+              type="text"
+              placeholder="search"
+              style={{ outline: "none", backgroundColor: "#F0F0F0 " }}
+            ></input>
+          </div>
+        </div>
+        <div className="flex items-center  w-[10%] justify-evenly">
+          <img src={cartIcon} alt="cart" className=" align-middle w-[20%]" />
+          <img
+            src={profileIcon}
+            alt="profile"
+            className=" align-middle w-[20%]"
+          />
+        </div>
+      </main>
+    </nav>
+  );
+};
+
+export default Navbar;
