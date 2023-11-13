aa9e8cdb387ec4e3f14642e8e0f4be3232cbb365
AgonoDavid <davidagono239@gmail.com>
2023-11-13 22:11:24 +0100

    ..

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,31 +8,35 @@ const Hero = () => {
       <main className="max-w-[1100px] m-auto flex justify-between items-center w-full">
         <div className="w-[50%]">
           <div className=" py-3">
-            <p className="font-Teko text-[90px] leading-[75px]">
+            <p className="font-Teko text-[90px] leading-[75px] pb-2">
               FIND CLOTHES THAT MATCHES YOUR STYLE
             </p>
-            <p className=" text-[14px]">
+            <p className=" text-[14px] font-Quicksand font-medium">
               Browse through our meticulously crafted garments, designed to
               bring out your individuality and cater to your sense of style
             </p>
           </div>
-          <Button bg={"black"} textColor={"white"}>
+          <Button bg={"black"} textColor={"white"} my={"10px"}>
             Shop Now
           </Button>
           <div className=" flex justify-between w-[90%] h-[85px] py-3 ">
             <div>
               <p className=" text-slate-950 font-semibold text-3xl">200+</p>
-              <p>International Brands</p>
+              <p className=" font-Quicksand text-[13px]">
+                International Brands
+              </p>
             </div>
             <Divider orientation="vertical" border={"1px solid #000000"} />
             <div>
               <p className=" text-slate-950 font-semibold text-3xl">2,000+</p>
-              <p>High-Quality Products</p>
+              <p className=" font-Quicksand text-[13px]">
+                High-Quality Products
+              </p>
             </div>
             <Divider orientation="vertical" border={"1px solid #000000"} />
             <div>
               <p className=" text-slate-950 font-semibold text-3xl">30,000+</p>
-              <p>Happy Customers</p>
+              <p className=" font-Quicksand text-[13px]">Happy Customers</p>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     fontFamily: {
       Teko: ["Teko"],
+      Quicksand: ["Quicksand"],
     },
   },
   plugins: [],
